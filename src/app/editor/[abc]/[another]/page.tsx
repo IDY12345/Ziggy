bcb5c96e1b3e2dfe348c97bb10a61c1c6ddd551a
IDY12345 <106066822+IDY12345@users.noreply.github.com>
2023-08-04 15:42:36 +0530

    Deleted Some Files

--- src/app/editor/[abc]/[another]/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function EditorSubPost(params:any){
-    console.log(params)
-    return <main>
-        <h1>Hello Sub Editor</h1>
-    </main>
-}
--- src/app/editor/[abc]/page.tsx
+++
@@ -1,8 +0,0 @@
-//'use client'
-
-export default function EditorPost(params:any){
-    console.log(params)
-    return <main>
-        <h1>Hello Editor</h1>
-    </main>
-}
